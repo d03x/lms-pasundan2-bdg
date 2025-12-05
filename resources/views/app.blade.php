9f28393449dd76a913ecbabfa58fabbc29bed35b
d03x <dadanhidyt@gmail.com>
2025-12-05 11:26:44 +0700

    feat: Add web push notification script and service worker for enhanced user engagement

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-
+        <script charset="UTF-8" src="//web.webpushs.com/js/push/b52f9623f9189286fa9b9371d05601eb_1.js" async></script>
         <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=inter:100,200,200i,300,300i,400,500,600,700,700i,800,800i,900|poppins:100,200,300,400,500,600,700,800" rel="stylesheet" />
         @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
